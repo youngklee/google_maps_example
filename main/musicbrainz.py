7c12edfaaa7ca1a7d421a852eb787fd18ae2d921
Damon Rousis <damon.rousis@gmail.com>
2015-06-07 14:18:03 -0500

    added musicbrainz api

---
+++ main/musicbrainz.py
@@ -0,0 +1,29 @@
+from connector import Connector
+from parameter import Input, Output
+from url import MusicBrainz
+
+class Music(Connector):
+
+    artist = Input(iotype='string')
+    release_group = Input(iotype='string')
+    release = Input(iotype='string')
+    recording = Input(iotype='string')
+    label = Input(iotype='string')
+
+    area = Output(iotype='string')
+    artist = Output(iotype='string')
+    event = Output(iotype='string')
+    instrument = Output(iotype='string')
+    label = Output(iotype='string')
+    recording = Output(iotype='string')
+    release = Output(iotype='float')
+    release_group = Output(iotype='string')
+    series = Output(iotype='string')
+    work = Output(iotype='string')
+    url = Output(iotype='string')
+
+    def __init__(self);
+
+        super(Music, self).__init__()
+        self.set_url(MusicBrainz)
+        self.set_parser('xml')
--- main/url.py
+++ main/url.py
@@ -1,1 +1,2 @@
 GoogleElevation = 'http://maps.googleapis.com/maps/api/elevation/json?locations=$latitude,$longitude'
+MusicBrainz = ''
