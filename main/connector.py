1bbb3496d3d07bfc370e0363eafb230991392844
Damon Rousis <damon.rousis@gmail.com>
2015-06-07 15:42:09 -0500

    added throttling and authorization framework

--- main/connector.py
+++ main/connector.py
@@ -12,13 +12,43 @@ class Connector(object):
 
     def __init__(self):
         self.results = ResultSet() #list of dictionaries containing results
-        self.requires_auth = False
-        self.url = None
+
+    def set_auth_type(self, auth_type):
+        """Set the authorization type for given web service
+        
+        Allowed types:
+        
+        anon: web service only provides anonymous access
+        login: user must be logged in to use the web service
+        mixed: web service allows both anonymous and user-based access
+        """
+        pass
+
+    def set_throttle(self, limit=None, units=None):
+        """Set the request rate for the given service
+
+        units: {'requests per second', 'requests per day', etc.}
+        """
+        self.delay = 0
+        self.max_requests = 1e16
+        self.made_requests = 0
+
+    def throttle(self, f):
+        """wrapper function for throttling web service requests"""
+        def wrapper(*args, **kwargs):
+            if self.made_requests < self.max_requests:
+                time.sleep(self.delay)
+                f(*args, **kwargs)
+                self.made_requests += 1
+            else:
+                raise Exception, 'maximum request limit reached'
+        return wrapper
 
     def set_url(self, url_str):
         self.url = Template(url_str)
 
     def set_parser(self, output_format):
+        """sets the object variable to the correct output stream parser"""
         self.output_parser = output_parsers.get(output_format, lambda x:x)
 
     def get_url_params(self):
@@ -28,6 +58,7 @@ class Connector(object):
                 p[attr_name] = getattr(attr_value, 'value')
         return p
 
+    @throttle
     def fetch(self, config):
         for key, val in config.iteritems():
             setattr(getattr(self, key), 'value', val)
