e2ea638b3e21cb5bb75f3e30989d05a31c862a3e
Young Lee <younglee.phd@gmail>
2015-06-12 20:05:39 -0500

    Added openweathermap.org to url

--- services/url.py
+++ services/url.py
@@ -1,2 +1,3 @@
 GoogleElevation = 'http://maps.googleapis.com/maps/api/elevation/json?locations=$latitude,$longitude'
 MusicBrainz = ''
+OpenWeatherMap = 'http://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude'
